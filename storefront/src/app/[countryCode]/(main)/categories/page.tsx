ad925e1fedb4087a5e0c26e811e7dc262e994a1d
Rafael Soares <107703258+rsoaresdev@users.noreply.github.com>
2024-11-08 15:03:46 +0000

    Fix design

--- storefront/src/app/[countryCode]/(main)/categories/page.tsx
+++ storefront/src/app/[countryCode]/(main)/categories/page.tsx
@@ -25,40 +25,36 @@ export default function Categories() {
 
   return (
     <div className="flex flex-col items-center text-center mb-16">
-      <span className="text-lg-regular text-gray-600 mb-6">
+      <span className="text-xl-regular text-gray-600 mb-6">
         Explore All Categories
       </span>
       <p className="text-2xl-regular text-ui-fg-base max-w-lg mb-12">
         Discover the perfect product by browsing our categories
       </p>
 
-      <div className="grid grid-cols-5 gap-x-8">
+      <div className="grid grid-cols-5 gap-x-8 w-full mt-4">
         {loading && (
           <div className="flex justify-center items-center w-full h-full col-span-5">
             <div className="animate-spin rounded-full border-t-4 border-b-4 border-gray-600 w-12 h-12"></div>
           </div>
         )}
         {!loading && categories.length === 0 && <span>No product categories found.</span>}
-        {!loading && categories.length > 0 && (
-          <div className="grid grid-cols-5 gap-x-4">
-            {categories.map((category, index) => (
-              <div 
-                key={category.id} 
-                className={`py-2 px-4 ${index % 5 !== 4 ? 'border-r border-gray-300' : ''} flex justify-center`}
-              >
-                <Link
-                  className="flex flex-col items-center group transition duration-300"
-                  href={`/categories/${category.handle}`}
-                >
-                  <span className="relative text-center">
-                    {category.name}
-                    <span className="absolute left-1/2 transform -translate-x-1/2 -bottom-1 w-3/4 h-[2px] bg-black scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
-                  </span>
-                </Link>
-              </div>
-            ))}
+        {!loading && categories.length > 0 && categories.map((category, index) => (
+          <div
+            key={category.id}
+            className={`py-4 px-6 flex justify-center ${index % 5 !== 4 ? 'border-r border-gray-300' : ''}`}
+          >
+            <Link
+              className="flex flex-col items-center group transition duration-300"
+              href={`/categories/${category.handle}`}
+            >
+              <span className="relative text-center">
+                {category.name}
+                <span className="absolute left-1/2 transform -translate-x-1/2 -bottom-1 w-3/4 h-[2px] bg-black scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-center"></span>
+              </span>
+            </Link>
           </div>
-        )}
+        ))}
       </div>
     </div>
   )
