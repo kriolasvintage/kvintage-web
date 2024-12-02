f8efe05035cd0c8b3751775fa3c51e35b7061a25
Rafael Soares <107703258+rsoaresdev@users.noreply.github.com>
2024-12-02 17:14:21 +0000

    Update some things

--- storefront/next.config.js
+++ storefront/next.config.js
@@ -29,6 +29,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "admin.kvintage.dk"
+      }
     ],
   },
   serverRuntimeConfig: {
